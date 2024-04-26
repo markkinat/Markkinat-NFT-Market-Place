7e79f3a9d20ab74da6fa763956252d086c7d8563
BanjoSamuelOladele <o.samuel@native.semicolon.africa>
2024-04-26 18:56:46 +0100

    still building

--- src/contracts/CollectionNFT.sol
+++ src/contracts/CollectionNFT.sol
@@ -2,13 +2,16 @@
 pragma solidity 0.8.20;
 
 import "@openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract CollectionNFT is ERC721URIStorage {
+contract CollectionNFT is ERC721URIStorage, Ownable {
     string private decription;
     string private baseUri;
 
-    constructor(string memory name, string memory symbol, string memory desc, string memory uri) ERC721(name, symbol) {
+    constructor(string memory name, string memory symbol, string memory desc, string memory uri, address _owner) ERC721(name, symbol) Ownable(_owner) {
         decription = desc;
         baseUri = uri;
     }
+
+    
 }
