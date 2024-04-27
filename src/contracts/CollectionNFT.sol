3d6759e2412be5c6f43f4ecd337fee3eaf82811e
BanjoSamuelOladele <o.samuel@native.semicolon.africa>
2024-04-27 02:00:53 +0100

    buyAsset

--- src/contracts/CollectionNFT.sol
+++ src/contracts/CollectionNFT.sol
@@ -8,29 +8,45 @@ contract CollectionNFT is ERC721URIStorage, Ownable {
 
     string private decription;
     string private baseUri;
-    mapping (address => uint256) private minterTokenId;
-    mapping (address owner => uint256[] tokenId) public userAssetsList; 
-    mapping (address => mapping (uint256 => Asset)) public userAssets;
-    uint256 public defaultAssetAmount = 0.30 ether;
+    mapping(address => uint256) private minterTokenId;
+    mapping(address owner => uint256[] tokenId) public userAssetsList;
+    mapping(address => mapping(uint256 => Asset)) public userAssets;
+    uint256 public defaultAssetAmount = 0.3 ether;
+    uint8 public rate_;
+    uint8 public royaltyRate;
+    address private protocol;
 
     Asset[] public collectionListedAssets;
 
-    struct Asset{
+    struct Asset {
         address _creator;
         address _newOwner;
         string desc;
-        uint price;
-        uint listedDate;
+        uint256 price;
+        uint256 listedDate;
         bool listed;
         uint256 tokenId;
     }
 
-    constructor(string memory name, string memory symbol, string memory desc, string memory uri, address _owner) ERC721(name, symbol) Ownable(_owner) {
+    constructor(
+        address _protocol,
+        string memory name,
+        string memory symbol,
+        string memory desc,
+        string memory uri,
+        address _owner
+    ) ERC721(name, symbol) Ownable(_owner) {
         decription = desc;
         baseUri = uri;
+        protocol = _protocol;
     }
 
-    function mint( address _minter ) external {
+    modifier  onlyFactory() {
+        require(true, "");
+        _;
+    }
+
+    function mint(address _minter, string calldata desc) external {
         uint256 _tokenId = ++minterTokenId[_minter];
         Asset storage asset = userAssets[_minter][_tokenId];
         require(asset._newOwner == address(0), "Something gone wrong");
@@ -39,14 +55,14 @@ contract CollectionNFT is ERC721URIStorage, Ownable {
         asset._newOwner = _minter;
         asset.price = defaultAssetAmount;
         asset.tokenId = _tokenId;
-        asset.desc = "";
+        asset.desc = desc;
     }
 
-    function getContractBalance() external onlyOwner view returns(uint256 _contractBalance){
+    function getContractBalance() external view onlyOwner returns (uint256 _contractBalance) {
         _contractBalance = address(this).balance;
     }
 
-    function listAsset(uint _amount, address _owner, uint256 _tokenId) external {
+    function listAsset(uint256 _amount, address _owner, uint256 _tokenId) external {
         Asset storage asset = userAssets[_owner][_tokenId];
         require(asset._creator != address(0), "Does not own this asset");
         require(asset._newOwner != _owner, "Does not own this asset to this address");
@@ -55,31 +71,42 @@ contract CollectionNFT is ERC721URIStorage, Ownable {
         asset.price = _amount * (1 ether);
         userAssetsList[_owner].push(_tokenId);
         collectionListedAssets.push(asset);
-        _approve(address(this), _tokenId, _owner);
+        // _approve(address(this), _tokenId, _owner);
     }
 
     function cancelListing(uint256 _tokenId, address _owner) external {
         Asset storage asset = userAssets[_owner][_tokenId];
         require(asset.listed, "Asset Not listed");
         (bool isProofed, uint256 _index) = checker(_owner, _tokenId);
-        require (isProofed, "Not owner by this user");
+        require(isProofed, "Not owner by this user");
         asset.listed = false;
-        Asset memory lastAsset = collectionListedAssets[collectionListedAssets.length - 1]; 
+        Asset memory lastAsset = collectionListedAssets[collectionListedAssets.length - 1];
         collectionListedAssets[_index] = lastAsset;
         collectionListedAssets.pop();
     }
 
-    function checker(address _owner, uint id) private view returns (bool status, uint index){
+    function checker(address _owner, uint256 id) private view returns (bool status, uint256 index) {
         uint256 totalListed = collectionListedAssets.length;
-        for(uint i = 0; i < totalListed; i++){
+        for (uint256 i = 0; i < totalListed; i++) {
             Asset memory asset = collectionListedAssets[i];
-            if(asset._newOwner == _owner && asset.tokenId == id ) return (status = true, index = i);
+            if (asset._newOwner == _owner && asset.tokenId == id) return (status = true, index = i);
         }
         return (status = false, index);
     }
 
+    function setCollectionSalesRate(uint8 _rate) external onlyOwner {
+        rate_ = _rate;
+    }
 
-    function buyAsset(uint _tokenId, address _newOwner) external payable {
+    function changeRoyaltyRate(uint8 _newRate) external onlyFactory {
+
+    }
+
+    function buyAsset(uint256 _amount, address _newOwner, uint256 index) external payable {
+        Asset storage asset = collectionListedAssets[index];
+        require(asset.listed, "Asset not listed");
+        require(asset.price <= _amount, "Did not reach the amout specified for sales");
+
 
     }
 }
--- src/contracts/MarkkinatNFTFactory.sol
+++ src/contracts/MarkkinatNFTFactory.sol
@@ -1,13 +1,11 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.20;
 
-import { IMarkkinatNFT } from "../interfaces/IMarkkinatNFT.sol";
-import { ERC721 } from "@openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import { CollectionNFT } from "./CollectionNFT.sol";
-
+import {IMarkkinatNFT} from "../interfaces/IMarkkinatNFT.sol";
+import {ERC721} from "@openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {CollectionNFT} from "./CollectionNFT.sol";
 
 contract MarkkinatNFTFactory {
-
     address private owner;
     IMarkkinatNFT private iMarkkinatNFT;
     uint256 public collectionId;
@@ -18,10 +16,21 @@ contract MarkkinatNFTFactory {
         iMarkkinatNFT = IMarkkinatNFT(_collection);
     }
 
-    function createCollection(address _creator, string calldata _name, string calldata symbol, string calldata desc, string memory uri) external {
+    function createCollection(
+        address _creator,
+        string calldata _name,
+        string calldata symbol,
+        string calldata desc,
+        string memory uri
+    ) external {
         CollectionNFT newCollection = new CollectionNFT(_name, symbol, desc, uri, _creator);
         collectionsNft[++collectionId] = address(newCollection);
     }
 
-    // function getCollection(uint256 _id) external 
+    modifier onlyGovernance{
+        require(true);
+        _;
+    }
+
+    // function getCollection(uint256 _id) external
 }
