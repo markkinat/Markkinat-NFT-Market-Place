19d268d70a59fbc59a2b19d5d472dd1b719f54cf
okste1234 <okste1234@gmail.com>
2024-06-03 13:12:57 +0100

    UPDATE: deploy on L chain

--- script/MarkkinatMarketPlace.s.sol
+++ script/MarkkinatMarketPlace.s.sol
@@ -6,14 +6,17 @@ import "../src/contracts/MarkkinatMarketPlace.sol";
 
 contract DeployMarkkinatMarketPlace is Script {
     address DAO_ADDRESS = 0xee53D67596baf6c437D399493Ac0499A1459c626;
-    address DAO_CONTRACT = 0xa2FB4316387988559132626df7f68F6A40eCa46d;
+    address DAO_CONTRACT = 0x93d4c34f89D9f705f1CA2cA0dc98176862F0b777;
 
     function setUp() public {}
 
     function run() external returns (MarkkinatMarketPlace) {
         vm.startBroadcast();
 
-        MarkkinatMarketPlace markkinatMarketPlace = new MarkkinatMarketPlace(DAO_CONTRACT, DAO_ADDRESS);
+        MarkkinatMarketPlace markkinatMarketPlace = new MarkkinatMarketPlace(
+            DAO_CONTRACT,
+            DAO_ADDRESS
+        );
 
         vm.stopBroadcast();
         return markkinatMarketPlace;
